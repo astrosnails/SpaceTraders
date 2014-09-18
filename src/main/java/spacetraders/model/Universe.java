39f3bc1f640afb29aaed3ed196aa14b73b634952
cchen396 <cchen396@gatech.edu>
2014-09-18 19:28:20 -0400

    updated scarcityLevel() method

--- src/main/java/spacetraders/model/Universe.java
+++ src/main/java/spacetraders/model/Universe.java
@@ -1,25 +1,62 @@
 package spacetraders.model;
-import java.util.random;
+import java.util.ArrayList;
+import java.util.Random;
+import spacetraders.model.Planet.ResourceLevel;
 
 public class Universe {
-	Planet planet;
+	ArrayList<Planet> planets;
 	double scarcity;
 	Random rand = new Random();
 
     public Universe(Planet planet, double scarcity) {
-    	this.planet = planet;
+    	planets.add(planet);
     	this.scarcity = scarcity * rand.nextDouble();
 
     	//this.planet.setResourceLevel(planet.getResourceLevel() * scarcity);
     	//LETS DISCUSS AS A GROUP HOW TO set resourcelevel
     }
 
-    public void setPlanet(Planet planet) {
-    	this.planet = planet;
+
+    private ResourceLevel scarcityLevel(Planet planet) {
+        ResourceLevel level = planet.getResourceLevel();
+        switch (level) {
+            case NOSPECIALRESOURCES:
+                break;
+            case MINERALRICH:
+                break;
+            case MINERALPOOR:
+                break;
+            case DESERT:
+                break;
+            case LOTSOFWATER:
+                break;
+            case RICHSOIL:
+                break;
+            case POORSOIL:
+                break;
+            case RICHFAUNA:
+                break;
+            case LIFELESS:
+                break;
+            case WEIRDMUSHROOMS:
+                break;
+            case LOTSOFHERBS:
+                break;
+            case ARTISTIC:
+                break;
+            case WARLIKE:
+                break;
+            default: 
+        }
+        return level;
     }
 
-    public Planet getPlanet() {
-    	return this.planet;
+    public void addPlanet(Planet planet) {
+    	planets.add(planet);
+    }
+
+    public Planet getPlanet(int index) {
+    	return planets.get(index);
     }
 
     public void setScarcity(double scarcity) {
