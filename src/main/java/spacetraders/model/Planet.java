0d26c6161e64a4cddd5bc3156a583ef4350e24e4
Saleh Hamadeh <salehhamadeh@gmail.com>
2014-09-17 22:21:46 -0400

    Add planet model (incomplete)

--- src/main/java/spacetraders/model/Planet.java
+++ src/main/java/spacetraders/model/Planet.java
@@ -1,5 +1,36 @@
 package spacetraders.model;
 
 public class Planet {
+    public static enum TechnologyLevel {
+        PRE_AGRICULTURE,
+        AGRICULTURE,
+        MEDIEVAL,
+        RENAISSANCE,
+        EARLY_INDUSTRIAL,
+        INDUSTRIAL,
+        POST_INDUSTRIAL,
+        HI_TECH
+    }
     
+    public static enum ResourceLevel {
+        NOSPECIALRESOURCES,
+        MINERALRICH,
+        MINERALPOOR,
+        DESERT,
+        LOTSOFWATER,
+        RICHSOIL,
+        POORSOIL,
+        RICHFAUNA,
+        LIFELESS,
+        WEIRDMUSHROOMS,
+        LOTSOFHERBS,
+        ARTISTIC,
+        WARLIKE
+    }
+    
+    private String name;
+    private Coordinates coordinates;
+    private TechnologyLevel technologyLevel;
+    private ResourceLevel resourceLevel;
+    private Resources resources;
 }
