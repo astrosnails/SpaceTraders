e0348a36189e41fa1c3bc1c3163e51990816e12d
cchen396 <cchen396@gatech.edu>
2014-09-18 17:16:50 -0400

    Updated Universe.java
    created the getters, setters, and constructor

--- src/main/java/spacetraders/model/Universe.java
+++ src/main/java/spacetraders/model/Universe.java
@@ -1,5 +1,33 @@
 package spacetraders.model;
+import java.util.random;
 
 public class Universe {
-    
+	Planet planet;
+	double scarcity;
+	Random rand = new Random();
+
+    public Universe(Planet planet, double scarcity) {
+    	this.planet = planet;
+    	this.scarcity = scarcity * rand.nextDouble();
+
+    	//this.planet.setResourceLevel(planet.getResourceLevel() * scarcity);
+    	//LETS DISCUSS AS A GROUP HOW TO set resourcelevel
+    }
+
+    public Planet setPlanet(Planet planet) {
+    	this.planet = planet;
+    }
+
+    public Planet getPlanet() {
+    	return this.planet;
+    }
+
+    public double setScarcity() {
+    	this.scarcity = scarcity;
+    }
+
+    public double getScarcity() {
+    	return this.scarcity;
+    }
+
 }
