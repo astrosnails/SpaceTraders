01fc12ba8da5eb4319d233a3e486400bd44185c8
Saleh Hamadeh <salehhamadeh@gmail.com>
2014-09-18 21:05:03 -0400

    Set max skill points to 15.

--- src/main/java/spacetraders/NewGameDialogController.java
+++ src/main/java/spacetraders/NewGameDialogController.java
@@ -37,6 +37,7 @@ import spacetraders.model.*;
  */
 public class NewGameDialogController implements Initializable {
 
+    private final int MAX_SKILL_POINTS = 15;
     private MainApplication application;
     
     @FXML
@@ -82,13 +83,13 @@ public class NewGameDialogController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         fighterSPSlider.valueProperty().addListener(
-            new LabelUpdaterOnSliderChangeListener(fighterSPLabel));
+            new LabelUpdaterOnSliderChangeListener(fighterSPSlider, fighterSPLabel));
         pilotSPSlider.valueProperty().addListener(
-            new LabelUpdaterOnSliderChangeListener(pilotSPLabel));
+            new LabelUpdaterOnSliderChangeListener(pilotSPSlider, pilotSPLabel));
         traderSPSlider.valueProperty().addListener(
-            new LabelUpdaterOnSliderChangeListener(traderSPLabel));
+            new LabelUpdaterOnSliderChangeListener(traderSPSlider, traderSPLabel));
         engineerSPSlider.valueProperty().addListener(
-            new LabelUpdaterOnSliderChangeListener(engineerSPLabel));
+            new LabelUpdaterOnSliderChangeListener(engineerSPSlider, engineerSPLabel));
     }
     
     public void setMainApplication(MainApplication application) {
@@ -98,14 +99,27 @@ public class NewGameDialogController implements Initializable {
     private class LabelUpdaterOnSliderChangeListener implements ChangeListener<Number> {
         private Label label;
 
-        public LabelUpdaterOnSliderChangeListener(Label label) {
+        public LabelUpdaterOnSliderChangeListener(Slider slider, Label label) {
             this.label = label;
         }
 
         @Override
         public void changed(ObservableValue<? extends Number> ov,
-            Number oldValue, Number newValue) {
-                label.setText(String.format("%.0f", newValue));
+                Number oldValue, Number newValue) {
+            int pointsUsed = 0;
+            pointsUsed += (int) fighterSPSlider.getValue();
+            pointsUsed += (int) pilotSPSlider.getValue();
+            pointsUsed += (int) traderSPSlider.getValue();
+            pointsUsed += (int) engineerSPSlider.getValue();
+            
+            int pointsRemaining = MAX_SKILL_POINTS - pointsUsed;
+            
+            fighterSPSlider.setMax((int)fighterSPSlider.getValue() + pointsRemaining);
+            pilotSPSlider.setMax((int)pilotSPSlider.getValue() + pointsRemaining);
+            traderSPSlider.setMax((int)traderSPSlider.getValue() + pointsRemaining);
+            engineerSPSlider.setMax((int)engineerSPSlider.getValue() + pointsRemaining);
+            
+            label.setText(String.format("%.0f", newValue));
         }
     }
 }
