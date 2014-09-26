71ae378b1c336b2727912b4bc56eb566385aec00
hifzasakhi <hifzasakhi@gmail.com>
2014-09-26 14:52:16 -0400

    added the add, remove, and contains methods for the Cargo.java for addition, removal, and querying of resources.

--- src/main/java/spacetraders/model/Cargo.java
+++ src/main/java/spacetraders/model/Cargo.java
@@ -16,4 +16,44 @@ public class Cargo {
 		} 
 		return false;
 	}
+
+	public boolean add(Resources addMe) {
+		if (a == null) {
+			return false;
+		} 
+		if (validateCargoSpace()) {
+			a.add(addMe);
+			return true;
+		}
+		return false;
+	}
+
+	public boolean contains(Resources seeMe) {
+		if (seeMe == null) {
+			return false;
+		} 
+		if (a.size() != 0) {
+			for (Resources x: a) {
+				if (x != null && x.equals(seeMe)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public boolean remove(Resources removeMe) {
+		if (removeMe == null) {
+			return false;
+		} 
+		if (contains()) {
+			for (Resources x: a) {
+				if (x != null && x.equals(removeMe)) {
+					a.remove(x);
+					return true;
+				}
+			}		
+		}
+		return false;
+	}
 }
