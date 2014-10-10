b62a8eabde5ebf2eb48f976461cf292ca79815dd
hifzasakhi <hifzasakhi@users.noreply.github.com>
2014-10-10 12:23:27 -0400

    java doc

--- src/main/java/spacetraders/Abstract/TravelListener.java
+++ src/main/java/spacetraders/Abstract/TravelListener.java
@@ -1,17 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package spacetraders.Abstract;
 
 import spacetraders.model.Planet;
 
 /**
- *
+ * The TravelListener interface.
  * @author saleh
  */
 public interface TravelListener {
+    /**
+    * Lets you travel.
+    * @param: Planet destination
+    * @return: none
+    */
     public void onTravel(Planet destination);
 }
