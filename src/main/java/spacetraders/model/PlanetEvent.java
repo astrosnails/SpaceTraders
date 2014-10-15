aec1a15ba8574f05640861570ca494896f799ae6
cchen396 <cchen396@gatech.edu>
2014-10-15 19:40:14 -0400

    created planet enum

---
+++ src/main/java/spacetraders/model/PlanetEvent.java
@@ -0,0 +1,14 @@
+package spacetraders.model;
+
+/* 
+* enumerated class for Planet Events
+* @author team 6
+* @version 1.0
+*/
+public enum PlanetEvent {
+    DROUGHT,
+    CROPFAIL,
+    WAR,
+    LACK_OF_WORKERS,
+    BOREDOM
+}
