8716694460783278bf830fe50e68ca9b41c72b3a
Saleh Hamadeh <salehhamadeh@gmail.com>
2014-11-11 16:45:48 -0500

    Setup Junit

---
+++ test/spacetraders/Tests/NewEmptyJUnitTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package spacetraders.Tests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import spacetraders.MainApplication;
+
+/**
+ *
+ * @author saleh
+ */
+public class NewEmptyJUnitTest {
+    
+    public NewEmptyJUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void getPlayerTest() {
+        MainApplication application = new MainApplication();
+        assertNull(application.getPlayer());
+    }
+}
